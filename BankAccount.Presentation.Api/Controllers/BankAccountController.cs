cf1388894571be567208e7cb10988de04d90e436
gustavo-pavan <Gustavo Pavan@DESKTOP-D9SACH7>
2023-04-27 23:58:23 -0300

    add new controller

---
+++ BankAccount.Presentation.Api/Controllers/BankAccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAccount.Presentation.Api.Controllers
+{
+    [Route("api/bank-account")]
+    [ApiController]
+    public class BankAccountController : ControllerBase
+    {
+    }
+}
